0dcda356e66706acfeaa901e471f70112c4b5859
Vadim2403 <vadim.pirog007@gmail.com>
2020-03-09 20:52:10 +0200

    design#2

--- Course Work/Areas/Admin/Controllers/AdminController.cs
+++ Course Work/Areas/Admin/Controllers/AdminController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -63,6 +65,29 @@ namespace Course_Work.Areas.Admin.Controllers
    
             }
             return RedirectToAction("Index", "Admin", new { area = "Admin" });
+        }
+        public ActionResult Email(string email)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("oasis.workua@gmail.com");
+                message.To.Add(new MailAddress(email));
+                message.Subject = "Інструкція";
+                var htmlString = $"Ти шо дибіл?";
+                message.IsBodyHtml = true;
+                message.Body = htmlString;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("oasis.workua@gmail.com", "Qwerty-1");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception) { return RedirectToAction("Index","Admin"); }
+            return RedirectToAction("Index", "Admin");
         }
     }
 }
