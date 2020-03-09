0c75fbb28ee12b864df621dd5d3740b421f171df
Vadim2403 <vadim.pirog007@gmail.com>
2020-03-09 19:03:48 +0200

    Sending instructions

--- Course Work/Controllers/HomeController.cs
+++ Course Work/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,6 +26,29 @@ namespace Course_Work.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        public ActionResult Email(string email)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("oasis.workua@gmail.com");
+                message.To.Add(new MailAddress(email));
+                message.Subject = "Інструкція";
+                var htmlString = $"Ти шо дибіл?";
+                message.IsBodyHtml = true;
+                message.Body = htmlString;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("oasis.workua@gmail.com", "Qwerty-1");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception) { return View(); }
             return View();
         }
     }
